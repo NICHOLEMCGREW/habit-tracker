fb4228395fdfb0cfcba6fddf9469c3fac2190fcd
Nichole McGrew <nicholemcgrew@gmail.com>
2024-02-22 13:15:26 -0600

    added habitsList and completed day

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = 3000;
+const port = 3002;
 const cors = require("cors");
 app.use(cors());
 
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose
-  .connect()
+  .connect("")
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -19,29 +19,57 @@ mongoose
     console.log("Error Connecting to MongoDb", error);
   });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running...`);
-});
+  app.listen(port, () => {
+    console.log("Server running on port 3000");
+  });
+  
+  const Habit = require("./models/habit");
 
-const Habit = require("./models/habit")
-
-//endpoint to create habit in backend
-
-app.post("/habits", async(req,res) => {
-  try{
-    const {title, color, repeatMode, reminder} = req.body;
+//endpoint to create a habit in the backend
+app.post("/habits", async (req, res) => {
+  try {
+    const { title, color, repeatMode, reminder } = req.body;
 
     const newHabit = new Habit({
-      title, 
+      title,
       color,
-      repeatedMode,
-      reminder
-    })
+      repeatMode,
+      reminder,
+    });
 
     const savedHabit = await newHabit.save();
-    res.status(200).json(savedHabit)
+    res.status(200).json(savedHabit);
+  } catch (error) {
+    res.status(500).json({ error: "Network error" });
+  }
+});
+
+app.get("/habitslist", async (req, res) => {
+  try {
+    const allHabits = await Habit.find({});
+
+    res.status(200).json(allHabits);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("habits/:habitID/completed/:day", async(req,res) => {
+  try{
+    const {habitId,day} = req.params;
+    const habit = await Habit.findById(habitId);
+
+    if(!habit){
+      return res.status(404).json({error:"Habit not found"})
+    }
+    habit.completed[day] = ture;
+
+    await habit.save();
+
+    res.status(200).json({message:"Habit completion status updated"})
 
   } catch(error){
-    res.status(500).json({error:"Network error"})
+    console.log("Error",error)
+    res.status(500).json({error:error.message})
   }
 })
--- app/home/create.js
+++ app/home/create.js
@@ -1,146 +1,188 @@
 import {
-	StyleSheet,
-	Text,
-	TextInput,
-	View,
-	TouchableOpacity,
-	Pressable,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  Alert,
 } from "react-native";
-import React from "react";
-import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { Ionicons, AntDesign } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
-import index from ".";
-// import { TouchableOpacity } from "react-native-web";
+import axios from "axios";
 
 const create = () => {
-	const colors = [
-		"#FF5733", // Red
-		"#FFD700", // Gold
-		"#5D76A9",
-		"#1877F2", // Medium Purple
-		"#32CD32", // Lime Green
-		"#CCCCFF", // Tomato
-		"#4169E1", // Royal Blue
-	];
-	const days = ["M", "T", "W", "T", "F", "S", "S"];
+  const [selectedColor, setSelectedColor] = useState("");
+  const [title, setTitle] = useState("");
+  const colors = [
+    "#FF5733", // Red
+    "#FFD700", // Gold
+    "#5D76A9",
+    "#1877F2", // Medium Purple
+    "#32CD32", // Lime Green
+    "#CCCCFF", // Tomato
+    "#4169E1", // Royal Blue
+  ];
+  const days = ["M", "T", "W", "T", "F", "S", "S"];
+  async function addHabit() {
+    try {
+      const habitDetails = {
+        title: title,
+        color: selectedColor,
+        repeatMode: "daily",
+        reminder: true,
+      };
 
-	return (
-		<View style={{ padding: 10 }}>
-			<Ionicons name="arrow-back" size={24} color="black" />
+      const response = await axios.post(
+        "http://localhost:3002/habits",
+        habitDetails
+      );
 
-			<Text style={{ fontSize: 20, marginTop: 10 }}>
-				Create <Text style={{ fontSize: 20, fontWeight: "500" }}>Habit</Text>
-			</Text>
-			<TextInput
-				style={{
-					width: "95%",
-					marginTop: 15,
-					padding: 15,
-					borderRadius: 10,
-					backgroundColor: "#E1EBEE",
-				}}
-				placeholder="Title"
-			/>
+      if (response.status === 200) {
+        setTitle("");
+        Alert.alert("Habit added successfully");
+      }
 
-			<View style={{ marginVertical: 10 }}>
-				<Text style={{ fontWeight: 18, fontWeight: "500" }}>Color</Text>
-				<View
-					style={{
-						flexDirection: "row",
-						alignItems: "center",
-						gap: 10,
-						marginTop: 10,
-					}}
-				>
-					{colors.map((item, index) => (
-						<TouchableOpacity key={index} activeOpacity={0.8}>
-							<FontAwesome name="square" size={30} color={item} />
-						</TouchableOpacity>
-					))}
-				</View>
-			</View>
+      console.log("habit added", response);
+    } catch (error) {
+      console.log("error adding a habit", error);
+    }
+  }
+  return (
+    <View style={{ padding: 10 }}>
+      <Ionicons name="arrow-back" size={24} color="black" />
 
-			<Text style={{ fontSize: 18, fontWeight: "500" }}>Repeat</Text>
-			<View
-				style={{
-					flexDirection: "row",
-					alignItems: "center",
-					gap: 10,
-					marginVertical: 10,
-				}}
-			>
-				<Pressable
-					style={{
-						backgroundColor: "#AFDBF5",
-						padding: 10,
-						borderRadius: 6,
-						flex: 1,
-					}}
-				>
-					<Text style={{ textAlign: "center" }}>Daily</Text>
-				</Pressable>
-				<Pressable
-					style={{
-						backgroundColor: "#AFDBF5",
-						padding: 10,
-						borderRadius: 6,
-						flex: 1,
-					}}
-				>
-					<Text style={{ textAlign: "center" }}>Weekly</Text>
-				</Pressable>
-			</View>
-			<Text style={{ fontSize: 18, fontWeight: "500" }}>On these days</Text>
-			<View
-				style={{
-					flexDirection: "row",
-					alignItems: "center",
-					gap: 10,
-					marginTop: 10,
-				}}
-			>
-				{days.map((item, index) => (
-					<Pressable
-						style={{
-							width: 20,
-							height: 40,
-							borderRadius: 5,
-							backgroundColor: "#E0E0E0",
-							justifyContent: "center",
-							alignItems: "center",
-						}}
-					>
-						<Text>{item}</Text>
-					</Pressable>
-				))}
-			</View>
+      <Text style={{ fontSize: 20, marginTop: 10 }}>
+        Create <Text style={{ fontSize: 20, fontWeight: "500" }}>Habit</Text>
+      </Text>
+      <TextInput
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        style={{
+          width: "95%",
+          marginTop: 15,
+          padding: 15,
+          borderRadius: 10,
+          backgroundColor: "#E1EBEE",
+        }}
+        placeholder="Title"
+      />
 
-			<View
-				style={{
-					marginTop: 20,
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
-				}}
-			>
-				<Text style={{ fontSize: 17, fontWeight: "500" }}>Reminder</Text>
-				<Text style={{ fontSize: 17, fontWeight: "500", color: "#2774AE" }}>
-					Yes
-				</Text>
-			</View>
+      <View style={{ marginVertical: 10 }}>
+        <Text style={{ fontSize: 18, fontWeight: "500" }}>Color</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 10,
+            marginTop: 10,
+          }}
+        >
+          {colors?.map((item, index) => (
+            <TouchableOpacity
+              onPress={() => setSelectedColor(item)}
+              key={index}
+              activeOpacity={0.8}
+            >
+              {selectedColor === item ? (
+                <AntDesign name="plussquare" size={30} color={item} />
+              ) : (
+                <FontAwesome name="square" size={30} color={item} />
+              )}
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
 
-			<Pressable
-				style={{
-					marginTop: 25,
-					backgroundColor: "#00428c",
-					padding: 10,
-					borderRadius: 8,
-				}}
-			>
-				<Text style={{textAlign:"center",color:"white",fontWeight:"bold"}}>SAVE</Text>
-			</Pressable>
-		</View>
-	);
+      <Text style={{ fontSize: 18, fontWeight: "500" }}>Repeat</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 10,
+          marginVertical: 10,
+        }}
+      >
+        <Pressable
+          style={{
+            backgroundColor: "#AFDBF5",
+            padding: 10,
+            borderRadius: 6,
+            flex: 1,
+          }}
+        >
+          <Text style={{ textAlign: "center" }}>Daily</Text>
+        </Pressable>
+        <Pressable
+          style={{
+            backgroundColor: "#AFDBF5",
+            padding: 10,
+            borderRadius: 6,
+            flex: 1,
+          }}
+        >
+          <Text style={{ textAlign: "center" }}>Weekly</Text>
+        </Pressable>
+      </View>
+
+      <Text style={{ fontSize: 18, fontWeight: "500" }}>On these days</Text>
+
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 10,
+          marginTop: 10,
+        }}
+      >
+        {days?.map((item, index) => (
+          <Pressable
+            style={{
+              width: 40,
+              height: 40,
+              borderRadius: 5,
+              backgroundColor: "#E0E0E0",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text>{item}</Text>
+          </Pressable>
+        ))}
+      </View>
+
+      <View
+        style={{
+          marginTop: 20,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text style={{ fontSize: 17, fontWeight: "500" }}>Reminder</Text>
+        <Text style={{ fontSize: 17, fontWeight: "500", color: "#2774AE" }}>
+          Yes
+        </Text>
+      </View>
+
+      <Pressable
+       onPress={addHabit}
+        style={{
+          marginTop: 25,
+          backgroundColor: "#00428c",
+          padding: 10,
+          borderRadius: 8,
+        }}
+      >
+        <Text
+          style={{ textAlign: "center", color: "white", fontWeight: "bold" }}
+        >
+          SAVE
+        </Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default create;
