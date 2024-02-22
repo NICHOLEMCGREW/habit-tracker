a6814636f5e835f155d43dcbf46781ff9f8a1579
Nichole McGrew <nicholemcgrew@gmail.com>
2024-02-22 12:28:15 -0600

    connected to db and created app.post enpoint to create habit in backend

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = 3002;
+const port = 3000;
 const cors = require("cors");
 app.use(cors());
 
@@ -11,14 +11,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose
-  .connect("mongodb+srv://nicholemcgrew:1HrXdwp9oDzyxutB@cluster0.lnzfbrm.mongodb.net/")
+  .connect()
   .then(() => {
     console.log("Connected to MongoDB");
   })
   .catch((error) => {
-    console.log("Error Connecting to MongoDB", error);
+    console.log("Error Connecting to MongoDb", error);
   });
 
-app.listen(port, () => {
-  console.log("Server running...");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running...`);
 });
+
+const Habit = require("./models/habit")
+
+//endpoint to create habit in backend
+
+app.post("/habits", async(req,res) => {
+  try{
+    const {title, color, repeatMode, reminder} = req.body;
+
+    const newHabit = new Habit({
+      title, 
+      color,
+      repeatedMode,
+      reminder
+    })
+
+    const savedHabit = await newHabit.save();
+    res.status(200).json(savedHabit)
+
+  } catch(error){
+    res.status(500).json({error:"Network error"})
+  }
+})
